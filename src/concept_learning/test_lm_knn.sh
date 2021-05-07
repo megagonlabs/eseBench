623a151d9278ee1461b5d93b743dd5c6dafc2def
ubuntu <ubuntu@ip-172-31-20-191.us-west-2.compute.internal>
2021-05-07 20:13:31 +0000

    kb extraction pipeline notebook added

--- src/concept_learning/test_lm_knn.sh
+++ src/concept_learning/test_lm_knn.sh
@@ -2,17 +2,17 @@
 chmod +x lm_concept_learn.sh
 algo=knn
 
-for data in tripadvisor 
+for data in imdb 
 do
-    for embed in meg-ac meg-pt
+    for embed in meg-ac
     do
-        for size in 20 50 100
+        for size in 20
         do
-            if [ "$embed" = "meg-pt" ]; then
-                ./lm_concept_learn.sh $algo $data $embed $size 3072 1
-            else
-                ./lm_concept_learn.sh $algo $data $embed $size 768 1
-            fi
+            #if [ "$embed" = "meg-pt" ]; then
+            #    ./lm_concept_learn.sh $algo $data $embed $size 3072 1
+            #else
+            ./lm_concept_learn.sh $algo $data $embed $size 768 1
+            #fi
         done
     done
 done
