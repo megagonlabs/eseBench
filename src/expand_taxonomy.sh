9d883838e8b8356d86f6e671fbace4d315a9c181
nikibhutani <nbhutani@umich.edu>
2022-03-10 12:55:53 -0800

    path correction

--- src/expand_taxonomy.sh
+++ src/expand_taxonomy.sh
@@ -13,6 +13,7 @@ module load slurm
 
 cmd1="/nfs/users/nikita/.conda/envs/kb_entity_expan/bin/python compute_keyphrase_embeddings.py -m bert-base-uncased -et ac -d ../../data/${DATA}/intermediate"
 cmd2="/nfs/users/nikita/.conda/envs/kb_entity_expan/bin/python add_seed_instances_embeddings.py -m bert-base-uncased -et ac -d ../../data/${DATA}/intermediate -b ../../data/${DATA}/ -c 750"
-cmd3="/nfs/users/nikita/.conda/envs/kb_entity_expan/bin/python expand_taxonomy.py -d ${DATA} --kdt"
+cmd3="cd .."
+cmd4="/nfs/users/nikita/.conda/envs/kb_entity_expan/bin/python expand_taxonomy.py -d ${DATA} --kdt"
 
-sbatch -G 1 -J kb_expan --ntasks-per-node=1 --nodes=1 -w node002 --wrap="$cmd1 && $cmd2 && $cmd3"
+sbatch -G 1 -J kb_expan --ntasks-per-node=1 --nodes=1 -w node002 --wrap="$cmd1 && $cmd2 && $cmd3 && $cmd4"
