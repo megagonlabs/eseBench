69cc2b38a9db788d72e5a96499b0fbb0937ef267
Sajjadur Rahman <srahman@Sajjadurs-MacBook-Pro.local>
2022-04-21 16:31:17 -0700

    release readme updated

--- src/expand_taxonomy.sh
+++ src/expand_taxonomy.sh
@@ -4,6 +4,6 @@ DATA=$2
 
 source activate $ENV_PATH
 
-${ENV_PATH}/bin/python compute_keyphrase_embeddings.py -m bert-base-uncased -et ac -d ../../data/${DATA}/intermediate
-${ENV_PATH}/bin/python add_seed_instances_embeddings.py -m bert-base-uncased -et ac -d ../../data/${DATA}/intermediate -b ../../data/${DATA}/ -c 750
+${ENV_PATH}/bin/python compute_keyphrase_embeddings.py -m bert-base-uncased -et ac -d ../data/${DATA}/intermediate
+${ENV_PATH}/bin/python add_seed_instances_embeddings.py -m bert-base-uncased -et ac -d ../data/${DATA}/intermediate -b ../data/${DATA}/ -c 750
 ${ENV_PATH}/bin/python expand_taxonomy.py -d ${DATA} --kdt
